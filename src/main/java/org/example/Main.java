7898f280433f038c91932ea354fe3406517a9f84
Harun Can Yildirim <eharuncan@gmail.com>
2022-11-23 17:22:18 +0300

    refactoring

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,12 +1,12 @@
 package org.example;
 
-import org.example.Problem6.Problem6;
+import org.example.Problem204.Problem204;
 
 public class Main {
 
     public static void main(String[] args) {
         System.out.println();
-        Problem6 problem = new Problem6();
+        Problem204 problem = new Problem204();
         System.out.println(problem.getName());
         System.out.println(problem.getUrl());
         System.out.println();
